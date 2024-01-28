179e9dd95a96c55c716ba14470ebd81cde69524e
anindo-kubuntu <sarkeranindo@gmail.com>
2024-01-28 11:29:19 +0600

    udpate dda line

--- js/algorithms/dda-line.js
+++ js/algorithms/dda-line.js
@@ -4,26 +4,26 @@ import { round } from "../utils";
 export function ddaLine(x0, y0, x1, y1) {
   setLabel(x0, y0, `ddaLine(${x0}, ${y0}, ${x1}, ${y1})`);
 
+  [x0, y0, x1, y1] = ensureInitialLessThanFinal(x0, y0, x1, y1);
+
   let dx = Math.abs(x1 - x0);
   let dy = Math.abs(y1 - y0);
 
   let m = dy / dx;
-  let xEnd = x0 > x1 ? x0 : x1;
-  let yEnd = y0 > y1 ? y0 : y1;
 
-  let x = x0 < x1 ? x0 : x1;
-  let y = y0 < y1 ? y0 : y1;
+  let x = x0;
+  let y = y0;
 
   setPixel(x, y);
 
   if (m <= 1) {
-    while (x <= xEnd) {
+    while (x <= x1) {
       setPixel(x, round(y));
       y = y + m;
       x++;
     }
   } else {
-    while (y <= yEnd) {
+    while (y <= y1) {
       setPixel(round(x), y);
       x = x + 1 / m;
       y++;
